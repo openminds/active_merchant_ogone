1504c98323f592b5c294672581930ec363138577
Simon Menke <simon@mrhenry.be>
2009-09-01 11:50:59 +0200

    Made secive_url's configurable

--- lib/active_merchant_ogone.rb
+++ lib/active_merchant_ogone.rb
@@ -8,39 +8,40 @@ module ActiveMerchant #:nodoc:
   module Billing #:nodoc:
     module Integrations #:nodoc:
       module Ogone
-        TEST_URL = 'https://secure.ogone.com/ncol/test/orderstandard.asp'
-        LIVE_URL = 'https://secure.ogone.com/ncol/prod/orderstandard.asp'
 
-        mattr_accessor :service_url
-        class << self
-          def service_url
-            mode = ActiveMerchant::Billing::Base.integration_mode
-            case mode
-            when :production
-              LIVE_URL
-            when :test
-              TEST_URL
-            else
-              raise StandardError, "Integration mode set to an invalid value: #{mode}"
-            end
-          end
-
-          def notification(post, options)
-            Notification.new(post, options)
-          end
-
-          def SHASign_out(fields, signature)
-            keys = ['orderID','amount','currency','PSPID']
-            datastring = keys.collect{|key| fields[key]}.join('')
-            Digest::SHA1.hexdigest("#{datastring}#{signature}").upcase
-          end
-
-          def SHASign_in(fields, signature)
-            keys = ['orderID','currency','amount','PM','ACCEPTANCE','STATUS','CARDNO','PAYID','NCERROR','BRAND']
-            datastring = keys.collect{|key| fields[key]}.join('')
-            Digest::SHA1.hexdigest("#{datastring}#{signature}").upcase
+        mattr_accessor :test_service_url
+        mattr_accessor :live_service_url
+        
+        self.test_service_url = 'https://secure.ogone.com/ncol/test/orderstandard.asp'
+        self.live_service_url = 'https://secure.ogone.com/ncol/prod/orderstandard.asp'
+        
+        
+        def self.service_url
+          mode = ActiveMerchant::Billing::Base.integration_mode
+          case mode
+          when :production then self.live_service_url
+          when :test       then self.test_service_url
+          else
+            raise StandardError, "Integration mode set to an invalid value: #{mode}"
           end
         end
+        
+        def self.notification(post, options)
+          Notification.new(post, options)
+        end
+        
+        def self.SHASign_out(fields, signature)
+          keys = ['orderID','amount','currency','PSPID']
+          datastring = keys.collect{|key| fields[key]}.join('')
+          Digest::SHA1.hexdigest("#{datastring}#{signature}").upcase
+        end
+        
+        def self.SHASign_in(fields, signature)
+          keys = ['orderID','currency','amount','PM','ACCEPTANCE','STATUS','CARDNO','PAYID','NCERROR','BRAND']
+          datastring = keys.collect{|key| fields[key]}.join('')
+          Digest::SHA1.hexdigest("#{datastring}#{signature}").upcase
+        end
+        
       end
     end
   end
